6dc68c524190c72b10707daa05100b4fa54ccac6
xiao fan <906111711@qq.com>
2022-11-21 21:36:23 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <router-view />dwd
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/login/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="login-container">
+        <el-form class="login-form">
+            <div class="title-container">
+                <h3 class="title">用户登录</h3>
+            </div>
+            <!-- username -->
+            <el-form-item>
+                <span class="svg-container">
+                    <el-icon>
+                        <Aim />
+                    </el-icon>
+                </span>
+                <el-input placeholder="username" name="username" type="text">
+                </el-input>
+            </el-form-item>
+            <!-- password -->
+            <el-form-item>
+                <span class="svg-container">
+                    <el-icon>
+                        <Aim />
+                    </el-icon>
+                </span>
+                <el-input placeholder="password" name="password">
+                </el-input>
+                <span class="show-pwd">
+                    <el-icon>
+                        <Aim />
+                    </el-icon>
+                </span>
+            </el-form-item>
+
+            <!-- 登录按钮 -->
+            <el-button type="primary" style="width: 100%; magin-bottom: 30px"></el-button>
+        </el-form>
+    </div>
+</template>
+
+<script setup>
+    import {} from 'vue'
+    import { Aim } from "@element-plus/icons-vue"
+
+</script>
+
+<style>
+    .login-container{
+        background-color: red;
+    }
+</style>

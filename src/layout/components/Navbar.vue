5b74b6b6b955b1f70e1cc4758904d4381e5a8b3e
xiao fan <906111711@qq.com>
2022-11-27 14:46:36 +0800

    rightlogo

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,10 +1,61 @@
 <template>
-    <div class="">Navbar</div>
+    <div class="navbar">
+        <div class="right-menu">
+            <!-- 头像 -->
+            <el-dropdown class="avatar-container" trigger="click">
+                <div class="avatar-wrapper">
+                    <el-avatar shape="square" :size="40" :src="rightLogo">
+                        <i class="el-icon-s-tools"></i>
+                    </el-avatar>
+                </div>
+                <template #dropdown>
+                    <el-dropdown-menu class="user-dropdown">
+                        <router-link to="/">
+                            <el-dropdown-item>主页</el-dropdown-item>
+                        </router-link>
+                        <a target="_blank" href="#">
+                            <el-dropdown-item>课程主页</el-dropdown-item>
+                        </a>
+                        <el-dropdown-item divided>退出登录</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </div>
+    </div>
 </template>
 
 <script setup>
-import {} from 'vue'
+import { } from 'vue'
+import rightLogo from '@/assets/rightLogo.png'
+
+
 </script>
 
 <style lang="scss" scoped>
+.navbar{
+    height: 50px;
+    overflow: hidden;
+    position: relative;
+    background-color: #fff;
+    box-shadow: 0 1px 4px rgba(0,21,41,.08);
+
+    .right-menu{
+        display: flex;
+        align-items: center;
+        float: right;
+        padding-right: 16px;
+
+        ::v-deep(.avatar-container){
+            cursor: pointer;
+            .avatar-wrapper {
+                margin-top: 5px;
+                position: relative;
+                .el-avatar {
+                    --el--avatar-background-color:none;
+                    margin-right: 12px;
+                }
+            }
+        }
+    }
+}
 </style>
